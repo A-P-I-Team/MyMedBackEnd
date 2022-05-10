750bc303fba15dfd2e62d44282f14cb6b3fb46ca
Alireza Haghani <alhaq.codeinstein@outlook.com>
2022-05-10 22:48:58 +0430

    Update URLs: Add Medicines & Prescriptions & PrescriptionMedicines URLs

--- volumes/app/DocAndPatient/urls.py
+++ volumes/app/DocAndPatient/urls.py
@@ -1,12 +1,19 @@
 from django.contrib import admin
-from django.urls import path,include,re_path
+from django.urls import path, include, re_path
 from .views import *
+from rest_framework.routers import DefaultRouter
 
+app_name = 'DocAndPatient'
 
-app_name='DocAndPatient'
+router = DefaultRouter()
+router.register('medicines', MedicineViewSet, basename='Medicines')
+router.register('prescriptions', PrescriptionViewSet, basename='Prescriptions')
+router.register('prescription-medicines', PrescriptionMedicinesViewSet, 'PrescriptionMedicines')
 
 urlpatterns = [
     path('my_doctors/', User_Doctors_List.as_view(), name='User_Doctors_List'),
     path('doctors/', ListCreateDoctor.as_view(), name='ListCreateDoctor'),
     path('doctors/<int:pk>/', RetriveDoctor.as_view(), name='RetriveDoctor'),
 ]
+
+urlpatterns += router.urls
