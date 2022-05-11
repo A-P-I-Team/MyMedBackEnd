b75fcdb211e2eecb62797df4ffbaaa600dfaad26
Mahdi <amirmahdishadman98@gmail.com>
2022-05-11 17:10:35 +0430

    update! requirements file

--- volumes/app/MyMed/settings.py
+++ volumes/app/MyMed/settings.py
@@ -93,10 +93,7 @@ WSGI_APPLICATION = 'MyMed.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-DATABASES = 
-
-
-{
+DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': os.environ.get('MARIADB_DATABASE'),
@@ -105,19 +102,15 @@ DATABASES =
         'HOST': os.environ.get('MARIADB_HOST'),
     }
 
-
-
-
-
 #     'default': {
 #         'ENGINE': 'django.db.backends.sqlite3',
 #         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-
-
-
+# }
 
 }
+
+
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
