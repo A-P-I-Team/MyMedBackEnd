e7533388af6f712609edd4287217a5b8ccd6b674
Alireza Haghani <alhaq.codeinstein@outlook.com>
2022-06-08 13:15:18 +0430

    Update IsDoctorOrReadOnly

--- volumes/app/DocAndPatient/permissions.py
+++ volumes/app/DocAndPatient/permissions.py
@@ -10,13 +10,12 @@ class IsAdminOrReadOnly(permissions.BasePermission):
 
 class IsDoctorOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
-        return True
-        # if request.method in permissions.SAFE_METHODS:
-        #     return True
-        # return bool(request.user and request.user.role == 'D')
+        # return True
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        return bool(request.user and request.user.role == 'D')
 
 
-# TODO: IsPrescriptionOfOwnerDoctor
 class IsPrescriptionOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
         return obj.patient == request.user or obj.doctor == request.user
