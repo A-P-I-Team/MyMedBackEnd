7b09ecd2fc2ce47f11fbc33da84e682d3a468bbc
Mahdi <amirmahdishadman98@gmail.com>
2022-06-10 10:24:03 +0430

    customize doctor and user model

--- volumes/app/DocAndPatient/admin.py
+++ volumes/app/DocAndPatient/admin.py
@@ -1,8 +1,17 @@
 from django.contrib import admin
 from .models import Doctor,Prescription,PrescriptionMedicines,Medicine,Reminder
 # Register your models here.
-admin.site.register(Doctor)
+# admin.site.register(Doctor)
 admin.site.register(Prescription)
 admin.site.register(PrescriptionMedicines)
 admin.site.register(Medicine)
 admin.site.register(Reminder)
+
+
+
+
+@admin.register(Doctor)
+class DoctorAdmin(admin.ModelAdmin):
+    list_display = ('username', 'first_name', 'last_name', 'ssn','gender','msn')
+    list_filter = ('gender', 'user_city','isVaccinated','degree','field',)
+    search_fields = ['username', 'first_name','last_name','about']
