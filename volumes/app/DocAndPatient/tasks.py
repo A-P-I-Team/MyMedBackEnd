55dbb2bddb1c18c8218448709dd5a4fc64fc3c35
Mahdi <amirmahdishadman98@gmail.com>
2022-07-09 15:08:36 +0430

    change tasks

--- volumes/app/DocAndPatient/tasks.py
+++ volumes/app/DocAndPatient/tasks.py
@@ -10,4 +10,5 @@ def Set_Reminder_Flase_After_Time():
     for item in myreminders:
         if(item.status == None):
             item.status=False
-    myreminders.save()
+            item.save()
+    
